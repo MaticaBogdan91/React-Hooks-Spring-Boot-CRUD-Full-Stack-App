dd98fedebd19e461c52cad60a022afd55308e0fd
Matica Bogdan <bogdan_maticutza@yahoo.com>
2022-04-15 02:26:21 +0300

    first commit

---
+++ src/components/HeaderComponent.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const HeaderComponents = () => {
+  return (
+    <div>
+        <header>
+            <nav className="navbar navbar-expand-md navbar-dark bg-dark">
+                <div>
+                    <a href= " https://javaguides.net" className= "navbar-brand">
+                        Employee Management Application
+
+                    </a>
+                </div>
+
+            </nav>
+
+        </header>
+
+    </div>
+  )
+}
+
+export default HeaderComponents
